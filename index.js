4d06a6d260dabcf0b516f3269e1857e080556e89
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 03:07:16 +0300

    Added createEmployeeRecords dunction

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+// Added createEmployeeRecords dunction 
+function createEmployeeRecords(arr) {
+    let lgArr = [];
+    arr.forEach(a => {
+        let t = createEmployeeRecord(a)
+        lgArr.push(t)
+    });
+    return lgArr;
+}
