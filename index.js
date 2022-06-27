5a33c91d8a3aced7f47441f217149934b4012d75
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-27 03:10:20 +0300

    Added hoursWorkedOnDate function

--- index.js
+++ index.js
@@ -42,3 +42,10 @@ function createTimeOutEvent(obj, dt) {
     obj.timeOutEvents.push(to)
     return obj;
 }
+
+//Added hoursWorkedOnDate func
+function hoursWorkedOnDate(obj, dt) {
+    let a  = obj.timeInEvents.find(a=>a.date === dt);
+    let b  = obj.timeOutEvents.find(a=>a.date === dt);
+    return (b.hour - a.hour) / 100;
+}
